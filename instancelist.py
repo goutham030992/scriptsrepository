da7ed3b160ea06814668b2b29e270c22d083c4d7
goutham030992 <goutham0992@gmail.com>
2018-03-30 01:48:15 +0000

    update py script to format teh host lists

--- instancelist.py
+++ instancelist.py
@@ -7,8 +7,8 @@ orig_stdout = sys.stdout
 f = open('hostlist.dat', 'w')
 sys.stdout = f
 
-access_key = "xxxxxx"
-secret_key = "xxxxxx"
+access_key = "xxxxx"
+secret_key = "xxxxx"
 
 client = boto3.client('ec2', aws_access_key_id=access_key, aws_secret_access_key=secret_key,
                                   region_name='us-east-1')
@@ -23,8 +23,9 @@ for region in ec2_regions:
         for tag in instance.tags:
           if tag["Key"] == 'Name':
             instancename = tag["Value"]
-        print (instance.private_ip_address,instancename,instance.id)
+        print (instance.private_ip_address + "\t" + instancename + "\t" + instance.id)
 
 
 sys.stdout = orig_stdout
 f.close()
+
